bf9cd47f7cdd71017be274713e04546349eae763
zero-wsh <15397608105@163.com>
2021-06-30 17:11:41 +0800

    改变mybatis执行sql语句方式

--- acTable-core/src/main/java/io/gitee/zerowsh/actable/dao/SqlServerMapper.java
+++ acTable-core/src/main/java/io/gitee/zerowsh/actable/dao/SqlServerMapper.java
@@ -2,9 +2,8 @@ package io.gitee.zerowsh.actable.dao;
 
 import io.gitee.zerowsh.actable.dto.ConstraintInfo;
 import io.gitee.zerowsh.actable.dto.TableColumnInfo;
-import io.gitee.zerowsh.actable.provider.SqlServerProvider;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -20,7 +19,7 @@ public interface SqlServerMapper {
      * @param tableName
      * @return
      */
-    @SelectProvider(type = SqlServerProvider.class, method = "isExistTable")
+    @Select("SELECT count(1) FROM sys.all_objects WHERE object_id = OBJECT_ID(#{tableName}) AND type IN ('U')")
     int isExistTable(@Param("tableName") String tableName);
 
     /**
@@ -28,7 +27,7 @@ public interface SqlServerMapper {
      *
      * @param sql
      */
-    @SelectProvider(type = SqlServerProvider.class, method = "executeSql")
+    @Select("${sql}")
     void executeSql(@Param("sql") String sql);
 
     /**
@@ -38,7 +37,20 @@ public interface SqlServerMapper {
      * @param tableName
      * @return
      */
-    @SelectProvider(type = SqlServerProvider.class, method = "getTableStructure")
+    @Select("SELECT d.name tableName,convert(nvarchar(255), f.value) tableComment,a.name columnName," +
+            " case when COLUMNPROPERTY( a.id,a.name,'IsIdentity')=1 then  1 else 0 end isAutoIncrement," +
+            " case when exists(SELECT 1 FROM sysobjects where xtype='PK' and parent_obj=a.id and name in (" +
+            " SELECT name FROM sysindexes WHERE indid in( SELECT indid FROM sysindexkeys WHERE id = a.id AND colid=a.colid))) then 1 else 0 end isKey," +
+            " b.name typeStr, COLUMNPROPERTY(a.id,a.name,'PRECISION') length," +
+            " isnull(COLUMNPROPERTY(a.id,a.name,'Scale'),0) decimalLength," +
+            " case when a.isnullable=1 then 1 else 0 end isNull,convert(nvarchar(255), e.text) defaultValue,convert(nvarchar(255), g.value) columnComment" +
+            " FROM syscolumns a" +
+            " left join systypes b on a.xusertype=b.xusertype" +
+            " inner join sysobjects d on a.id=d.id  and d.xtype='U' and  d.name<>'dtproperties'" +
+            " left join syscomments e on a.cdefault=e.id" +
+            " left join sys.extended_properties g on a.id=G.major_id and a.colid=g.minor_id" +
+            " left join sys.extended_properties f on d.id=f.major_id and f.minor_id=0" +
+            " where d.name=#{tableName}")
     List<TableColumnInfo> getTableStructure(@Param("tableName") String tableName);
 
     /**
@@ -48,7 +60,15 @@ public interface SqlServerMapper {
      * @param tableName
      * @return
      */
-    @SelectProvider(type = SqlServerProvider.class, method = "getConstraintInfo")
+    @Select("WITH MO_Cook AS\t(SELECT  IDX.NAME AS constraintName, IDX.TYPE_DESC AS constraintType,COL.NAME AS constraintColumnName,case when IDX.IS_PRIMARY_KEY = 1 then 1 else case when \t\t\t\t\t\t\tIDX.IS_UNIQUE_CONSTRAINT = 1 then 2 else 3 end end constraintFlag FROM  SYS.INDEXES IDX JOIN\n" +
+            "                SYS.INDEX_COLUMNS IDXCOL ON (IDX.OBJECT_ID = IDXCOL.OBJECT_ID AND IDX.INDEX_ID = IDXCOL.INDEX_ID) JOIN\n" +
+            "                SYS.TABLES TAB ON (IDX.OBJECT_ID = TAB.OBJECT_ID) JOIN\n" +
+            "                SYS.COLUMNS COL ON (IDX.OBJECT_ID = COL.OBJECT_ID AND IDXCOL.COLUMN_ID = COL.COLUMN_ID)\n" +
+            "                where  TAB.NAME=#{tableName})\n" +
+            "\tselect constraintName,constraintType,constraintFlag,stuff((select ','+constraintColumnName from  MO_Cook  \n" +
+            "            where c.constraintName=constraintName and c.constraintType=constraintType and c.constraintFlag=constraintFlag order by constraintColumnName\n" +
+            "            for xml path('')),1,1,'') as constraintColumnName  from MO_Cook c   \n" +
+            "\t\t\t\t\t\tgroup by c.constraintName,c.constraintType,c.constraintFlag")
     List<ConstraintInfo> getConstraintInfo(@Param("tableName") String tableName);
 
     /**
@@ -58,6 +78,8 @@ public interface SqlServerMapper {
      * @param tableName
      * @return
      */
-    @SelectProvider(type = SqlServerProvider.class, method = "getDefaultInfo")
+    @Select("select t.name constraintName,syscolumns.name constraintColumnName from (SELECT sysobjects.name,sysobjects.id FROM\tsysobjects \n" +
+            "where\tsysobjects.id IN ( SELECT syscolumns.cdefault FROM sysobjects INNER JOIN syscolumns ON sysobjects.Id= syscolumns.Id WHERE sysobjects.name= #{tableName} ))t \n" +
+            "LEFT JOIN syscolumns ON t.Id= syscolumns.cdefault")
     List<ConstraintInfo> getDefaultInfo(@Param("tableName") String tableName);
 }

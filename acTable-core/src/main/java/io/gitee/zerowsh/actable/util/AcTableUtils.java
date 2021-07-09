d25a019647145935874586225495d27880ed3bb7
zero <254353372@qq.com>
2021-07-09 13:59:10 +0800

    优化

--- acTable-core/src/main/java/io/gitee/zerowsh/actable/util/AcTableUtils.java
+++ acTable-core/src/main/java/io/gitee/zerowsh/actable/util/AcTableUtils.java
@@ -5,18 +5,18 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import io.gitee.zerowsh.actable.config.CreateTableConfig;
 import io.gitee.zerowsh.actable.dao.BaseDatabaseMapper;
-import io.gitee.zerowsh.actable.dao.SqlServerMapper;
 import io.gitee.zerowsh.actable.emnus.DatabaseTypeEnums;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
-
-import static io.gitee.zerowsh.actable.emnus.DatabaseTypeEnums.SQL_SERVER;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 所有数据库共用工具类
@@ -26,28 +26,37 @@ import static io.gitee.zerowsh.actable.emnus.DatabaseTypeEnums.SQL_SERVER;
 @Component
 @Slf4j
 public class AcTableUtils {
+
+    @Resource
+    private BaseDatabaseMapper baseDatabaseMapper;
+    @Resource
+    private CreateTableConfig createTableConfig;
+
     /**
      * 执行脚本
      *
      * @return 是否执行成功
      */
-    public boolean executeScript(CreateTableConfig createTableConfig, BaseDatabaseMapper baseDatabaseMapper) {
+    public void executeScript() {
         DatabaseTypeEnums databaseType = createTableConfig.getDatabaseType();
         String script = createTableConfig.getScript();
         if (StrUtil.isBlank(script)) {
-            return false;
+            return;
         }
         log.info("执行 [{}] 初始化数据。。。", databaseType);
-        org.springframework.core.io.Resource[] resources;
-        try {
-            resources = new PathMatchingResourcePatternResolver().getResources(ResourceUtils.CLASSPATH_URL_PREFIX + script);
-        } catch (IOException e) {
-            log.warn("[{}] 没找到初始化数据文件 [{}]！！！", databaseType, script);
-            return false;
+        List<org.springframework.core.io.Resource> list = new ArrayList<>();
+        for (String s : script.split(StringPool.COMMA)) {
+            try {
+                list.addAll(Arrays.asList(new PathMatchingResourcePatternResolver().getResources(ResourceUtils.CLASSPATH_URL_PREFIX + s)));
+            } catch (IOException e) {
+                log.warn("[{}] 没找到初始化数据文件 [{}]！！！", databaseType, s);
+                return;
+            }
         }
+
         String fileUrl = null;
         try {
-            for (org.springframework.core.io.Resource resource : resources) {
+            for (org.springframework.core.io.Resource resource : list) {
                 File file = resource.getFile();
                 if (file.isFile()) {
                     fileUrl = file.getAbsolutePath();
@@ -60,9 +69,9 @@ public class AcTableUtils {
             }
         } catch (IOException e) {
             log.warn("执行 [{}] 初始化数据失败！！！读取文件异常 [{}]", databaseType, fileUrl);
-            return false;
+            return;
         }
-        return true;
+        log.info("执行 [{}] 初始化数据完成！！！", databaseType);
     }
 
     /**

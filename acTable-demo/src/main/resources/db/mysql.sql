cd02c2dc490b3732916c441f124b12f3d2079344
zero-wsh <15397608105@163.com>
2021-07-23 14:57:16 +0800

    优化mysql插入数据的写法

--- acTable-demo/src/main/resources/db/mysql.sql
+++ acTable-demo/src/main/resources/db/mysql.sql
@@ -1,4 +1,2 @@
-INSERT INTO t_zero(`id`, `name`, `update_time`, `create_time`)
-SELECT 1, '1', '2021-07-16 08:53:10', '2021-07-16 08:53:13' FROM DUAL WHERE NOT EXISTS(SELECT 1 FROM t_zero WHERE id = '1');
-INSERT INTO t_zero(`id`, `name`, `update_time`, `create_time`)
-SELECT 2, '1', '2021-07-16 08:53:10', '2021-07-16 08:53:14' FROM DUAL WHERE NOT EXISTS(SELECT 1 FROM t_zero WHERE id = '2');
+INSERT IGNORE  INTO t_zero(`id`, `name`, `update_time`, `create_time`) values(1, '1', '2021-07-16 08:53:10', '2021-07-16 08:53:13');
+INSERT IGNORE  INTO t_zero(`id`, `name`, `update_time`, `create_time`) values(2, '2', '2021-07-16 08:53:10', '2021-07-16 08:53:14');

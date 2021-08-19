ab6d32d5b8714f7dfc47e57c7eabdf83d24482b0
zero <254353372@qq.com>
2021-08-19 11:34:02 +0800

    处理初始化语句

--- acTable/src/main/java/io/gitee/zerowsh/actable/constant/StringConstants.java
+++ acTable/src/main/java/io/gitee/zerowsh/actable/constant/StringConstants.java
@@ -14,10 +14,4 @@ public interface StringConstants {
     String LEFT_SQ_BRACKET = "[";
     String RIGHT_SQ_BRACKET = "]";
     String SEMICOLON = ";";
-    String QUOTATION = "'";
-    String QUOTE = "\"";
-    String BACK_SLASH = "\\";
-    String INSERT = "insert";
-    String UPDATE = "update";
-    String DELETE = "delete";
 }

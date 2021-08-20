11cbebc474480033ce804d082de5d1c1af42aad9
zero-wsh <254353372@qq.com>
2021-08-20 09:11:50 +0800

    初始化数据脚本执行逻辑优化

--- acTable-demo/src/main/resources/db/sql_server.sql
+++ acTable-demo/src/main/resources/db/sql_server.sql
@@ -1,4 +1,10 @@
-IF NOT EXISTS (SELECT 1 FROM t_zero WHERE name = '111')
-INSERT INTO [dbo].[t_zero] ([name], [create_time], [update_time]) VALUES ( N'111', '2021-07-07 17:25:17', '2021-07-07 17:25:20');
+IF NOT EXISTS (SELECT 1 FROM t_zero WHERE name = '111') /*
+
+IF NOT EXISTS (SELECT 1 FROM t_zero WHERE name = '222')
+INSERT INTO [dbo].[t_zero] ([name], [create_time], [update_time]) VALUES ( N'222', '2021-07-07 17:25:18', '2021-07-07 17:25:21');dfasdfa --fda dfasdf'"
+*/
+INSERT INTO [dbo].[t_zero] ([name], [create_time],
+[update_time]) VALUES ( N'111', '2021-07-07 17:25:17',
+'2021-07-07 17:25:20');
 IF NOT EXISTS (SELECT 1 FROM t_zero WHERE name = '222')
 INSERT INTO [dbo].[t_zero] ([name], [create_time], [update_time]) VALUES ( N'222', '2021-07-07 17:25:18', '2021-07-07 17:25:21');

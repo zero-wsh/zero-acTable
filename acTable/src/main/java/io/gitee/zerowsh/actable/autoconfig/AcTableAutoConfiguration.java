d9eda65b3d0dede6f29a760a332cb5c820070743
zero <254353372@qq.com>
2021-12-28 10:03:25 +0800

    自动注入

---
+++ acTable/src/main/java/io/gitee/zerowsh/actable/autoconfig/AcTableAutoConfiguration.java
@@ -0,0 +1,27 @@
+package io.gitee.zerowsh.actable.autoconfig;
+
+import io.gitee.zerowsh.actable.properties.AcTableProperties;
+import io.gitee.zerowsh.actable.service.AcTableService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zero
+ */
+@Configuration
+@EnableConfigurationProperties(AcTableProperties.class)
+@ConditionalOnClass(DataSource.class)
+public class AcTableAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AcTableService acTableService(DataSource dataSource, AcTableProperties acTableProperties) {
+        return new AcTableService(dataSource, acTableProperties);
+    }
+
+}
